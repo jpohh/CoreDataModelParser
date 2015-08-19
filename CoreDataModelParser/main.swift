7fbfd57eb45e2eb8c066ccbd4f31a763ca5b7089
James O'Leary <jpo@amburapp.com>
2015-08-19 17:28:45 -0400

    Initial Commit

---
+++ CoreDataModelParser/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CoreDataModelParser
+//
+//  Created by James O'Leary on 8/19/15.
+//  Copyright © 2015 James O'Leary. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
